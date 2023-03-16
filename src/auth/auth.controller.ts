289fcd19f02cc750a598488f9bf5c3fc637d0242
Asen <asen.trichkov1@gmail.com>
2023-03-17 01:36:41 +0200

    Initial

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import {
+    Controller,
+    Post
+} from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    signup() {
+        return 'Welcome'
+    }
+
+    @Post('signin')
+    signin(){
+        return 'Hello world'
+    }
+}
